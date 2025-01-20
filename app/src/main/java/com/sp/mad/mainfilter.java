73d3d780e941767e88074b78a328987ffcb6f62c
whxwhx04 <hongxiang17.23@ichat.sp.edu.sg>
2025-01-20 12:06:44 +0800

    update on filter and create listing page

---
+++ app/src/main/java/com/sp/mad/mainfilter.java
@@ -0,0 +1,14 @@
+package com.sp.mad;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class mainfilter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainfilter);
+    }
+}
